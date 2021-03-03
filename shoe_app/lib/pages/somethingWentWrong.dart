b99a4c40e4e576c3615bf0a04d3207dde65c9f9c
Shalini <shalinijeyra@gmail.com>
2021-03-03 10:33:00 +0530

    home page changes

--- shoe_app/lib/pages/somethingWentWrong.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class SomethingWentWrong extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
-}
